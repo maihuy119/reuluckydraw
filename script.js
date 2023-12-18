929d6a9f53d332a609ef7400cc4c55f4604149b3
maihuy119 <53705887+maihuy119@users.noreply.github.com>
2023-12-18 11:53:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-
+var selectedPerson;
+var excludedIds = [];
 
 function randomName() {
-    // Hiển thị tên ngẫu nhiên trên trang
-    var xhr = new XMLHttpRequest();
-      var jsonFilePath = 'list.json';
+  // Hiển thị tên ngẫu nhiên trên trang
+  var xhr = new XMLHttpRequest();
+  var jsonFilePath = 'list.json';
 
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          var jsonData = JSON.parse(xhr.responseText);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonData = JSON.parse(xhr.responseText);
+      var persons = jsonData;
 
-          // Chọn tên ngẫu nhiên
-          var randomIndex = Math.floor(Math.random() * jsonData.length);
-          var randomName = jsonData[randomIndex].name;
-          var randomDepart = jsonData[randomIndex].depart;
+      var filteredPersons = persons.filter(function (person) {
+        return !excludedIds.includes(person.id);
+      });
 
-          // Hiển thị tên ngẫu nhiên trong phần tử có id là "displayRandomName"
-          document.getElementById('name').innerText = randomName;
-          document.getElementById('depart').style.color = '#000';
-          document.getElementById('depart').innerText = randomDepart;
-        }
-      };
+      // Chọn tên ngẫu nhiên
+      var randomIndex = Math.floor(Math.random() * filteredPersons.length);
+      selectedPerson = filteredPersons[randomIndex];
 
-      xhr.open('GET', jsonFilePath, true);
-      xhr.send(); 
+      // Hiển thị tên ngẫu nhiên trong phần tử có id là "displayRandomName"
+      document.getElementById('name').innerText = selectedPerson.name;
+      document.getElementById('depart').style.color = '#000';
+      document.getElementById('depart').innerText = selectedPerson.depart;
+    }
+  };
+
+  xhr.open('GET', jsonFilePath, true);
+  xhr.send();
 }
 
-function roll(){  
-    var roll = setInterval(randomName, 50);
-    setTimeout(function() {
-        clearInterval(roll); // Dừng setInterval
-      }, 5000);
+function roll() {
+  document.getElementById('roll').style.visibility = 'hidden';
+  document.getElementById('clear').style.display = 'none';
+  var roll = setInterval(randomName, 5);
+  setTimeout(function () {
+    clearInterval(roll); // Dừng setInterval
+    document.getElementById('roll').innerText = 'Tiếp tục';
+    document.getElementById('roll').style.visibility = 'visible';
+    document.getElementById('clear').style.display = 'inline-block';
+  }, 5000);
+}
+
+function clearSelect() {
+  if (!excludedIds.includes(selectedPerson.id)) {
+    excludedIds.push(selectedPerson.id);
+    document.getElementById('clear').style.display = 'none';
+  }
 }
